ad65400b0d5979d4e3e9dddfc296713aa4a7919d
Mitsichury <mitsichury@gmail.com>
2022-08-06 17:47:13 +0200

    fix defintion

--- src/client/RegistryClient.ts
+++ src/client/RegistryClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export interface RegistryClient {
-  getPeersFromRegistry(): Promise<any>;
+  getPeersFromRegistry(): Promise<string[]>;
 
   sendIpToRegistry(ip: string): Promise<void>;
 
