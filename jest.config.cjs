f3a5a0ebedb3c0f5a0aaeb13e6e0816c1b7bbee0
Mitsichury <mitsichury@gmail.com>
2022-08-13 19:00:17 +0200

    clean

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,20 +3,17 @@ module.exports = {
     preset: "ts-jest",
     moduleFileExtensions: ["ts", "js", "cjs", "mjs"],
     transform: {
-        "^.+\\.(ts|tsx|js)$": "ts-jest",
+        "^.+\\.(ts|tsx)$": "ts-jest",
         "node_modules/tiny-secp256k1/lib/cjs/.+\\.(js|ts|cjs|mjs)$": "ts-jest",
         "node_modules/uint8array-tools/src/cjs.+\\.(js|ts|cjs|mjs)$": "ts-jest",
-        "node_modules/public-ip/.*": "ts-jest",
     },
     moduleNameMapper: {
         "^uint8array-tools$": "uint8array-tools/src/cjs",
         "^tiny-secp256k1$": "tiny-secp256k1/lib/cjs",
-        // "^public-ip$": "public-ip",
     },
     transformIgnorePatterns: [
         "node_modules/(?!tiny-secp256k1/lib/cjs/.*)",
         "node_modules/(?!uint8array-tools/src/cjs/.*)",
-        "node_modules/(?!public-ip/.*)",
     ],
     testPathIgnorePatterns: ["node_modules", "dist"],
     collectCoverageFrom: [
