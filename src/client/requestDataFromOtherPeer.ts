d0fa1b83447074c0aef1e8314d8bd07c4d3609da
Mitsichury <mitsichury@gmail.com>
2022-11-29 22:21:15 +0100

    add todo

--- src/client/requestDataFromOtherPeer.ts
+++ src/client/requestDataFromOtherPeer.ts
@@ -16,7 +16,8 @@ export async function requestDataFromOtherPeer(peersFromRegistry: string[], data
             const { orders } = await new NodeIndexer(peerUrl).getOrders();
             
             const toAdd: Record<string, IndexedOrder> = Object.values(orders).reduce((indexedOrders, indexedOrderResponse) => {
-                const indexedOrder: Record<string, IndexedOrder> = {};                 
+                const indexedOrder: Record<string, IndexedOrder> = {};  
+                //TODO Extract dedicated mapper :)
                 indexedOrder[indexedOrderResponse.hash!] =  new IndexedOrder(mapAnyToDbOrder(indexedOrderResponse), indexedOrderResponse.addedOn, indexedOrderResponse.hash);
                 return {...indexedOrders, indexedOrder};                
             },{});
