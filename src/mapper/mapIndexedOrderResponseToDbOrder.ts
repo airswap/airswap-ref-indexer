9020224bfcd78fb19ab2d86322f2f6af37b37718
Mitsichury <mitsichury@gmail.com>
2022-12-04 13:23:11 +0100

    finished

--- src/mapper/mapIndexedOrderResponseToDbOrder.ts
+++ src/mapper/mapIndexedOrderResponseToDbOrder.ts
@@ -4,10 +4,11 @@ import { mapAnyToDbOrder } from './mapAnyToDbOrder.js';
 
 
 function mapIndexedOrderResponseToDbOrder(indexedOrderResponse: IndexedOrderResponse): Record<string, IndexedOrder> | undefined {
-    const { order, hash, addedOn} = indexedOrderResponse;    
-    if(!order || !hash || !addedOn) {
-        return undefined;
-    }
+    if(!indexedOrderResponse) return undefined;
+
+    const { order, hash, addedOn} = indexedOrderResponse;
+    if(!order || !hash || !addedOn) return undefined;
+    
     const indexedOrder: Record<string, IndexedOrder> = {};  
     indexedOrder[hash] = new IndexedOrder(mapAnyToDbOrder(order), addedOn, hash);
     return indexedOrder;
@@ -15,7 +16,7 @@ function mapIndexedOrderResponseToDbOrder(indexedOrderResponse: IndexedOrderResp
 
 export function mapAllIndexedOrderResponseToDbOrder(orders: Record<string, IndexedOrderResponse>): Record<string, IndexedOrder> {
     const mapped: Record<string, IndexedOrder> = Object.values(orders).reduce((indexedOrders, indexedOrderResponse) => {
-        const indexedOrder = mapIndexedOrderResponseToDbOrder(indexedOrderResponse);                
+        const indexedOrder = mapIndexedOrderResponseToDbOrder(indexedOrderResponse);
         return indexedOrder ? {...indexedOrders, ...indexedOrder} : indexedOrders;
     }, {});
     return mapped;
